18bb0a8b691d37a9698a1b6b1d31717cb0fccee3
Andrew Durber <aidurber@gmail.com>
2017-01-04 20:26:02 +0000

    Meh

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,53 @@
 'use strict';
+const TILE_CLASS = 'title_card';
 
-console.log('\'Allo \'Allo! Content script');
+class DomTools{
+	constructor(props) {
+	}
+
+	getTiles(){
+		return document.getElementsByClassName(TILE_CLASS);
+	}
+}
+
+
+
+class RenderResults{
+	constructor(props) {
+		this.domTools = new DomTools();
+		this.tiles = this.domTools.getTiles();
+	}
+
+	render(){
+		this.tiles.forEach((tile) =>{
+			console.log(tile.getAttribute("aria-label"))
+		})
+	}
+
+
+}
+
+class Clarity{
+	constructor(props) {
+		this.valid = this.isNetflix();
+		this.renderer = new RenderResults();
+	}
+
+	load(){
+		if(!this.valid){
+			return;
+		}
+		this.renderer.render();
+	}
+	host(){
+		return window.location.hostname;
+	}
+	//Private
+	isNetflix(){
+		console.log(this.host())
+		return /(netflix)/.test(this.host());
+	}
+}
+
+let app = new Clarity();
+app.load();
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,22 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+	describe('Core', function () {
+		it('should not run if not netflix', function () {
+			var instance = new Clarity();
+			expect(instance.valid).to.equal(false);
+		});
 
-      });
-    });
-  });
+		it('should run if netflix', function () {
+			Clarity.prototype.host = function(){
+				return "www.netflix.com";
+			}
+
+			var instance = new Clarity();
+			expect(instance.valid).to.equal(true);
+		});
+
+			
+
+	});
 })();
